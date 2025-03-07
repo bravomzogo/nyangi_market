5f1275a05ce47c05173ac7cf258496e6af1ee964
bravomzogo <143389661+bravomzogo@users.noreply.github.com>
2025-03-07 03:12:39 +0300

    Defined project root

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,7 +123,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
