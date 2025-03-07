66e4ea94955969d4c958a7f23235d48f06161a86
bravomzogo <bravomzogo@gmail.com>
2025-03-07 17:51:05 +0300

    alfa

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('STAFF', 'Staff'),
+        ('CUSTOMER', 'Customer'),
+    ]
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='CUSTOMER')
+
+    def __str__(self):
+        return self.username
 
 
 # List of Tanzanian regions
