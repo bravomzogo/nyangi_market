d82000adb88b7b67ed0c87124da8656b189b7de9
Raxan7 <manyerere201@gmail.com>
2025-07-03 18:03:01 +0300

    Removed duplicate admin urls

--- online_market/urls.py
+++ online_market/urls.py
@@ -7,7 +7,6 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    path('admin/', admin.site.urls),
     path('mpesa-callback/', views.mpesa_callback, name='mpesa_callback'),
 
 ]
